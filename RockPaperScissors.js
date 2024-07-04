5590c06dae483c391174acb451078705f2a32e56
Alanah Haynes <alanahihaynes@gmail.com>
2024-07-04 16:23:07 -0400

    Created code for the User's Choice and the Computer's Choice

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,1 +1,22 @@
-console.log('What is up, World');
+
+function getComputerChoice(){
+    let choice= Math.floor(Math.random() * 3);
+    console.log('The Computer chose: ');
+    switch(choice){
+        case 0:
+            console.log('Rock');
+            break;
+        case 1:
+            console.log('Paper');
+            break;
+        case 2:
+            console.log('Scissors');
+            break;
+    }
+
+}
+
+function getHumanChoice(){
+    let userChoice= prompt("Enter Rock, Paper or Scissors: ");
+    console.log('You chose: '+ userChoice);
+}
